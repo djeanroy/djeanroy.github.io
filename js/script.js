291bef8009031210d09a55d7e45f59a3e8af9433
Damien <damien.jeanroy@epitech.eu>
2020-09-26 16:09:49 +0200

    push

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slide-section').click(function(e){
+        var linkHref = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top
+        }, 1000);
+        e.preventDefault();
+    });
+});
